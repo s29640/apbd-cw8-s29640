198075d93e0afcc91d173965d1469f986975f7cd
Robert Sendys <s29640@pjwstk.edu.pl>
2026-06-16 23:45:09 +0200

    Zainicjuj projekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace APBD_cw8_s29640.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
